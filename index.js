fed6f257f7cfc9673f0dbbc5b30f21b446412484
Bartosz Marciniec <bmarciniec@dynak.com>
2021-09-30 16:49:51 +0200

    fix args bug

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 const ks = require('node-key-sender');
-const argv = require("./argv")
+const argv = require('./argv');
 
-const keys = argv.keys?.split(",") ?? ['w', 's', 'a', 'd', 'space'];
-const baseTime = Math.round(argv.base) ?? 180;
-const maxRandTime =  Math.round(argv.randTime) ?? 60;
- 
-if (isNaN(baseTime) || isNaN(maxRandTime) ) {
+const keys = argv.keys?.split(',') ?? ['w', 's', 'a', 'd', 'space'];
+const baseTime = argv.base ?? 180;
+const maxRandTime = argv.randTime ?? 60;
+
+if (isNaN(baseTime) || isNaN(maxRandTime)) {
   throw new Error('podaj liczbe cymbale');
 }
-
 const pressRandomKey = () => {
-  const time =
-    (Math.floor(Math.random() * maxRandTime) + baseTime) * 1000;
+  const time = (Math.floor(Math.random() * maxRandTime) + baseTime) * 1000;
   const randomKey = keys[Math.floor(Math.random() * keys.length)];
   setTimeout(() => {
     ks.sendKey(randomKey);
